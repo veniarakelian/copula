e3d364eaf899c6f39e2c11a741da9c0bac9fbb01
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2019-12-05 18:06:54 +0200

    fixing-birth-3

--- copula/birth.py
+++ copula/birth.py
@@ -34,7 +34,7 @@ def birth(currentModel, u, dist, numbrk, q):
             while j <= 3 and G == 0:
                 if np.all(temp[j:] != np.ones(shape=(1,2)) * q[:numbrk - j2 - 1]):
                     G = 1
-                    temp = []
+                    temp[j:] = []
 
                 j += 1
 
@@ -42,7 +42,7 @@ def birth(currentModel, u, dist, numbrk, q):
             row = np.random.uniform(low=1 , high=2)
 
             if a < 1/3:
-                Q[numbrk - j2: numbrk - j2 + 1] = temp[row:]
+                Q[numbrk - j2: numbrk - j2 + 1] = temp[row:].H
             else:
                 if a >= 1/3 and a < 2/3:
                     Q[numbrk - j2: numbrk - j2 + 1] = np.fliplr(temp[row:]).H
@@ -50,20 +50,104 @@ def birth(currentModel, u, dist, numbrk, q):
                     if a >= 2/3:
                         Q[numbrk - j2: numbrk - j2 + 1] = np.ones(shape=(2,1)) * q[numbrk - j2]
 
-            #???#
-            Q[d - j2 + 2: numbrk + 1] = np.ones(shape=(1,1)) * Q(d- j2 + 1)
-            s = np.array([q[numbrk - j2], Q[numbrk - j2]: numbrk - j2 + 1])
+            Q[d - j2 + 2: numbrk + 1] = np.ones(shape=(1,1)) * Q[d- j2 + 1]
+            s = np.concatenate((q[numbrk - j2], Q[numbrk - j2: numbrk - j2 + 1]), axis=0)
 
         else:
+            if kn < np.min(t2):
+                Q = np.zeros(shape=(numbrk + 1, 1))
+                Q[3:numbrk + 1] = q[2:numbrk]
+                temp = combinations([1, 2, 3], 2)
+                j = 1
+                G = 0
+                while j <= 3 and G == 0:
+                    if np.all(temp[j:] != np.ones(shape=(1,2)) * q[0]):
+                        G = 1
+                        temp[j:] = []
+
+                    j += 1
+
+                a = np.random.uniform()
+                row = np.random.uniform(low=1 , high=2)
+
+                if a < 1/3:
+                    Q[:2] = temp[row:].H
+                else:
+                    if a >= 1/3 and a < 2/3:
+                        Q[:2] = np.fliplr(temp[row:]).H
+                    else:
+                        if a >= 2/3:
+                            Q[:2] = np.ones(shape=(2,1)) * q[1]
+
+                s = np.concatenate((q[1], Q[1:2]), axis=0)
+            else:
+                Q[:d - j2 - 1] = q[:d - j2 - 1]
+                temp = combinations([1, 2, 3], 2)
+                j = 1
+                G = 0
+                while j <= 3 and G == 0:
+                    if np.all(temp[j:] != np.ones(shape=(1,2)) * q[d - j2]):
+                        G = 1
+                        temp[j:] = []
+
+                    j += 1
+
+                a = np.random.uniform()
+                row = np.random.uniform(low=1 , high=2)
+
+                if a < 1/3:
+                    Q[d - j2:d - j2 + 1] = temp[row:].H
+                else:
+                    if a >= 1/3 and a < 2/3:
+                        Q[d - j2:d - j2 + 1] = np.fliplr(temp[row:]).H
+                    else:
+                        if a >= 2/3:
+                            Q[d - j2:d - j2 + 1] = np.ones(shape=(2,1)) * q[d - j2]
+
+                Q[d - j2 + 2: numbrk + 1] = q[d - j2 + 1:numbrk] 
+                s = np.concatenate((q[d - j2], Q[d - j2:d - j2 + 1]), axis=0)
+
+    elif j == numbrk and (np.any(np.absolute(k * np.ones(shape=(L+j,1)) - new)) <= dist* np.ones(shape=(L + j,1))) == 0:
+        z = 1
+        kn = k
+        new[j][0] = kn
+        bir = np.sort(new) 
+
+        temp = combinations([1, 2, 3], 2)
+        j = 1
+        G = 0
+
+        while j <= 3 and G == 0:
+            if np.all(temp[j:] != np.ones(shape=(1,2)) * q[0]):
+                G = 1
+                temp[j:] = []
+
+            j += 1
+
+        a = np.random.uniform()
+        row = np.random.uniform(low=1 , high=2)
+
+        if a < 2/3:
+            d = np.random.uniform()
+            if d < 1/2:
+                Q[1] = temp[row:][0]
+                Q[1:numbrk + 1] = np.ones(shape=(numbrk,1))  * temp([row][1])
+            else:
+                Q[1] = temp[row:][1]
+                Q[1:numbrk + 1] = np.ones(shape=(numbrk,1))  * temp([row][0])
+        else:
+            Q = q
+
+        s = np.concatenate((q[0], Q[:2]), axis=0)
 
 
+    elif (np.any(np.absolute(k * np.ones(shape=(L+j,1)) - new)) <= dist* np.ones(shape=(L + j,1))) == 0:
+        z = -3
+        bir = currentModel
+        kn = k
+        Q = q
+        s = 0
 
-    bir = 0
-    kn = 0
-    s = 1
-    Q = 1
-    q = 2
-    z = 3
     result = {"bir": bir, "kn": kn, "s": s, "Q": Q, "q": q, "z": z}
 
 
