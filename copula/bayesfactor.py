108eddb6faa979b3fd30adcd0950748fc3304a65
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2020-01-08 12:49:00 +0200

    Fix-bayes-factor

--- copula/bayesfactor.py
+++ copula/bayesfactor.py
@@ -1,27 +1,15 @@
+from allclayton import allclayton
+from allfrank import allfrank
+from allgumbel import allgumbel
 
-from scipy.stats import norm, expon
-from scipy.optimize import minimize
-from scipy.linalg import det, inv
-from math import pi
-import numpy as np
-from allnorm import allnorm
-from copulalib.copulalib import Copula
-from bayesfactor import bayesfactor
+def bayesfactor(x, y, copula):
 
-def bayesfactor(x,y,thetastart,copula)
+    if copula==1:
+        BFu = allclayton(x,y)["BFu"]
+    else:
+        if copula==2:
+            BFu = allfrank(x,y)["BFu"]
+        else:
+            BFu = allgumbel(x,y)["BFu"]
 
-if copula==1
-    [~,~,~,~,~,BFu]=allclayton(x,y,thetastart)
-else
-    if copula==2
-        [~,~,~,~,~,BFu]=allfrank(x,y,thetastart)
-    else
-        [~,~,~,~,~,BFu]=allgumbel(x,y,thetastart)
-    #end
-#end
-
-return BFu
-
-# den kserw pws na periorisw kai na parw mono to BFu apo ta results
-# result = {"theta": theta, "cop1": cop1, "hes": hes, "hes_prior_cor": hes_prior_cop, "BF": BF, "BFu": BFu}
-#   return result
+    return BFu
