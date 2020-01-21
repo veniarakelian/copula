ac2772fee0f5713b61fe99c147b41643b8b4b71d
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2020-01-21 17:49:01 +0200

    birth-ok

--- copula/birth.py
+++ copula/birth.py
@@ -1,7 +1,6 @@
 import numpy as np
 from itertools import combinations
 from pandas import read_excel
-from numpy.linalg import inv
 
 def birth(currentModel, u, dist, numbrk, q):
 
@@ -13,7 +12,7 @@ def birth(currentModel, u, dist, numbrk, q):
     k = np.random.randint(low=dist, high=sample - dist + 1)
     w = np.random.uniform()
 
-    if j < numbrk and not np.all(np.any(np.absolute(k * np.ones(shape=(L+j)) - new)  <= dist * np.ones(shape=(L+j,1)))):
+    if j < numbrk and not np.all(np.any(np.absolute(k * np.ones(shape=(L+j)) - new)  <= dist * np.ones(shape=(L+j, 1)))):
         z = 1
         kn = k
         new[j - 1] = kn
@@ -28,6 +27,7 @@ def birth(currentModel, u, dist, numbrk, q):
             Q = np.ones(numbrk + 1)
             Q[:numbrk - j2 - 1] = q[:numbrk - j2 - 1]
             temp = [comb for comb in combinations([1, 2, 3], 2)]
+
             j = 0
             G = 0
 
@@ -40,19 +40,19 @@ def birth(currentModel, u, dist, numbrk, q):
                 j += 1
 
             a = np.random.uniform()
-            row = np.random.randint(low=1 , high=3)
+            row = np.random.randint(low=1, high=3)
 
             if a < 1.0/3:
                 Q[numbrk - j2 - 1:numbrk - j2 + 1] = list(temp[row - 1])
             else:
                 if a >= 1.0/3 and a < 2.0/3:
-                    Q[numbrk - j2 - 1: numbrk - j2 + 1] = list(temp[row - 1])[::-1]
+                    Q[numbrk - j2 - 1:numbrk - j2 + 1] = list(temp[row - 1])[::-1]
                 else:
                     if a >= 2.0/3:
-                        Q[numbrk - j2 - 1: numbrk - j2 + 1] = q[numbrk - j2 - 1]
+                        Q[numbrk - j2 - 1:numbrk - j2 + 1] = q[numbrk - j2 - 1]
 
             Q[d - j2:numbrk + 1] = Q[d - j2]
-            s = np.concatenate((np.asarray([q[numbrk - j2 - 1]]), Q[numbrk - j2 - 1: numbrk - j2 + 1]), axis=0)
+            s = np.concatenate((np.asarray([q[numbrk - j2 - 1]]), Q[numbrk - j2 - 1:numbrk - j2 + 1]), axis=0)
         else:
             if kn < np.min(t2):
                 Q = np.zeros(numbrk + 1)
@@ -69,7 +69,7 @@ def birth(currentModel, u, dist, numbrk, q):
                     j += 1
 
                 a = np.random.uniform()
-                row = np.random.randint(low=1 , high=3)
+                row = np.random.randint(low=1, high=3)
 
                 if a < 1.0/3:
                     Q[:2] = list(temp[row - 1])
@@ -80,7 +80,7 @@ def birth(currentModel, u, dist, numbrk, q):
                         if a >= 2.0/3:
                             Q[:2] = q[0]
 
-                s = np.concatenate((np.asarray([q[0]]), Q[:2]))
+                s = np.concatenate((np.asarray([q[0]]), Q[:2]), axis=0)
 
             else:
                 Q = np.zeros(numbrk + 1)
@@ -97,7 +97,7 @@ def birth(currentModel, u, dist, numbrk, q):
                     j += 1
 
                 a = np.random.uniform()
-                row = np.random.randint(low=1 , high=3)
+                row = np.random.randint(low=1, high=3)
 
                 if a < 1.0/3:
                     Q[d - j2 - 1:d - j2 + 1] = list(temp[row - 1])
@@ -112,6 +112,7 @@ def birth(currentModel, u, dist, numbrk, q):
                 s = np.concatenate((np.asarray([q[d - j2 - 1]]), Q[d - j2 - 1:d - j2 + 1]), axis=0)
 
     elif j == numbrk and not np.all(np.any(np.absolute(k * np.ones(shape=(L+j)) - new)  <= dist * np.ones(shape=(L+j,1)))):
+        k = 132
         z = 1
         kn = k
         new[j - 1] = kn
@@ -129,7 +130,7 @@ def birth(currentModel, u, dist, numbrk, q):
             j += 1
 
         a = np.random.uniform()
-        row = np.random.randint(low=1 , high=3)
+        row = np.random.randint(low=1, high=3)
 
         if a < 2.0/3:
             d = np.random.uniform()
@@ -155,11 +156,9 @@ def birth(currentModel, u, dist, numbrk, q):
 
     return result
 
-# Test #
 if __name__ == "__main__":
 
-    df = read_excel("/home/petropoulakis/Desktop/artificial_data_iosif.xlsx", sheet_name='Sheet1') #, header=None)
-
+    df = read_excel("/home/petropoulakis/Desktop/artificial_data_iosif.xlsx", sheet_name='Sheet1')
     u = []
 
     for index, row in df.iterrows():
