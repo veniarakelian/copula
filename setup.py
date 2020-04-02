fbc5e0361b843bc18eb51f1bc9c60c43d7158bc3
PetropoulakisPanagiotis <petropoulakispanagiotis@gmail.com>
2020-04-02 14:09:42 +0300

    setup

--- setup.py
+++ setup.py
@@ -25,6 +25,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
     ]
 )
